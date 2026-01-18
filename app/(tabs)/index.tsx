c1c1091dfb967c03b88aa866fd52e1a24003dddd
Hari <hariharank7112001@gmail.com>
2026-01-18 22:47:46 +0530

    added header and input component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,30 @@
 // import { useTheme } from "@/hooks/useTheme";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
 import { useTheme } from "@/hooks/useTheme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
-  
+  const { toggleDarkMode, colors } = useTheme();
+  const styles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={toggleDarkMode}>
-        <Text>Toggle</Text>
-      </TouchableOpacity>
-   
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Header />
+          <TodoInput />
+          <TouchableOpacity onPress={toggleDarkMode}>
+            <Text>Toggle</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // flexDirection: "row",
-    // gap: 10,
-    // justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: "black",
-  },
-  text: {
-    color: "#ffffff",
-  },
-  button: {
-    padding: 20,
-    borderRadius: 10,
-    backgroundColor: "lightblue",
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,21 +2,18 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 });
 export default function RootLayout() {
   return (
     <ConvexProvider client={convex}>
-    <ThemeProvider>
-      <SafeAreaView style={{ flex: 1 }}>
+      <ThemeProvider>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           {/* <Stack.Screen name="about" options={{ title: "about" }} /> */}
         </Stack>
-      </SafeAreaView>
-    </ThemeProvider>
+      </ThemeProvider>
     </ConvexProvider>
   );
 }
